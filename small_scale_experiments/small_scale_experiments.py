3434f067ad3e8e0c783c223cfa06854b95b9e040
Jinyu Zhao <zhaojinyu19@163.com>
2020-05-04 14:50:48 -0700

    update small scale experiments

--- small_scale_experiments/small_scale_experiments.py
+++ small_scale_experiments/small_scale_experiments.py
@@ -6,9 +6,63 @@ import torch
 from model import *
 from util import *
 from sklearn import datasets
+import os
 
 
 device = "cuda"
+
+print("###################EXPERIMENTS ON GLASS##########################")
+with open("data/glass.data") as f:
+    raw_data = f.readlines()
+data = []
+target = []
+for i in range(len(raw_data)):
+    line = raw_data[i].split(',')
+    data.append(line[1:-1])
+    target.append(int(line[-1]))
+data = np.array(data).astype(np.float)
+target = np.array(target)
+model = VaDE(6, 3, 9)
+if  os.path.exists('./pretrained_parameters/parameters_glass.pth'):
+    model.load_state_dict(torch.load('./pretrained_parameters/parameters_glass.pth'))
+else:
+    train_loader = []
+    cla = target
+    for i in range(data.shape[0]):    
+        train_loader.append(torch.from_numpy(data[i:i+1]).float())
+    model.pre_train(train_loader, 10)
+    train(model, train_loader, 100)
+    torch.save(model.state_dict(), "pretrained_parameters/parameters_glass.pth")
+
+mean, _ = model.encoder(torch.from_numpy(data).float())
+Z_vade = linkage(mean.detach().numpy(), "average")
+rootnode, nodelist = scipy.cluster.hierarchy.to_tree(Z_vade, rd=True)
+print("Dendrogram Purity:", compute_purity(Z_vade, cla, 6))
+
+
+print("###################EXPERIMENTS ON DIGIT###########################")
+
+data, target = datasets.load_digits(n_class=10, return_X_y=True)
+train_loader = []
+cla = target
+for i in range(data.shape[0]//10):    
+    train_loader.append(torch.from_numpy(data[i*10:(i+1)*10]).float())
+
+model = VaDE(10, 3, 64)
+
+if  os.path.exists('./pretrained_parameters/parameters_digits.pth'):
+    model.load_state_dict(torch.load('./pretrained_parameters/parameters_digits.pth'))
+else:
+    model.pre_train(train_loader, 50)
+    train(model, train_loader, 100)
+    torch.save(model.state_dict(), "pretrained_parameters/parameters_digits.pth")
+
+mean, _ = model.encoder(torch.from_numpy(data).float())
+Z_vade = linkage(mean.detach().numpy()[:200], "average")
+rootnode, nodelist = scipy.cluster.hierarchy.to_tree(Z_vade, rd=True)
+print("Dendrogram Purity:", compute_purity(Z_vade, cla[:200], 10))
+
+
 print("###################EXPERIMENTS ON COVERTYPE#########################")
 cover_data, cover_targets = datasets.fetch_covtype(data_home=None, download_if_missing=True, random_state=None, shuffle=False, return_X_y=True)
 print(cover_data.shape, cover_targets.shape)
@@ -21,22 +75,3 @@ model = VaDE(7, 10, 54).to(device)
 model.pre_train(train_loader, 50)
 train(model, train_loader, 100)
 torch.save(model.state_dict(), "covertype_VaDE_parameters.pth")
-
-print("###################EXPERIMENTS ON DIGIT###########################")
-
-data, target = datasets.load_digits(n_class=10, return_X_y=True)
-train_loader = []
-cla = target
-for i in range(data.shape[0]//10):    
-    train_loader.append(torch.from_numpy(data[i*10:(i+1)*10]).float())
-
-model = VaDE(10, 3, 64)
-model.pre_train(train_loader, 50)
-train(model, train_loader, 100)
-
-Z_vade = linkage(mean.detach().numpy()[:200], "average")
-rootnode, nodelist = scipy.cluster.hierarchy.to_tree(Z_vade, rd=True)
-print("Dendrogram Purity:", compute_purity(Z_vade, cla[:200], 10))
-
-
-print("###################EXPERIMENTS ON GLASS##########################")

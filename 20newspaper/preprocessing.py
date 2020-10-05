6c89e77430b841f55a00a58fbe15154609a6c88c
jiz077 <jiz077@jiz077-19909.pods.jiz077.svc.cluster.local>
2020-10-04 23:59:37 -0700

    update preprocess

--- 20newspaper/preprocessing.py
+++ 20newspaper/preprocessing.py
@@ -14,14 +14,16 @@ outputs2, pooled2 = encoder(**tokens_pt2)
 import torch
 embedding = torch.Tensor([])
 
-encoder = AutoModel.from_pretrained(MODEL_NAME)
-tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+for j in range(100):
+    print("{}/{} finished".format(j, 100))
 
-for i in range(1000):
-    tokens_pt2 = tokenizer(newsgroups_train.data[i][:512], return_tensors="pt", padding=True, max_length=2048, truncation=True)
-    outputs2, pooled2 = encoder(**tokens_pt2)
-    embedding = torch.cat([embedding, pooled2])
-    
-print(embedding.shape)
+    encoder = AutoModel.from_pretrained(MODEL_NAME)
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+
+    for i in range(100):
+        tokens_pt2 = tokenizer(newsgroups_train.data[i][:512], return_tensors="pt", padding=True)
+        _, pooled2 = encoder(**tokens_pt2)
+        embedding = torch.cat([embedding, pooled2])
+    print(embedding.shape)
 
 torch.save(embedding, "bert_embedding.pt")
